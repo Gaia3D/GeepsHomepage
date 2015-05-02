4946cae403dd51aabf67a2836bfc70be8c6afd23
BJ Jang <jangbi882@gmail.com>
2015-05-02 10:50:44 +0900

    배너 스크롤 완료

--- Design/js/geeps_home.js
+++ Design/js/geeps_home.js
@@ -1,31 +1,70 @@
 /*
 ** 이벤트 처리
 */
+$(window).load(onLoad);
+
+function onLoad() {
+	//startRotator(".rolling");
+    scroller = new Scroller(".rolling", "scroller", 5000);
+	$(".button").bind( "click", onClick);
+	$(".button").bind( "mouseenter", onMouseEnter);
+	$(".button").bind( "mouseleave", onMouseLeave);
+}
+
+function onClick(event) {
+	var name = event.currentTarget.name;
+    switch (name) {
+        case "prev":
+            scroller.rotateForward(".rolling");
+            break;
+        case "next":
+            scroller.rotateBackward(".rolling");
+            break;
+    }
+}
+
+function onMouseEnter(event) {
+	
+}
+
+function onMouseLeave(event) {
+	
+}
 
 /*
 ** 배너 회전
 */
-function rotateForward(elem) {
-	var first = $(elem+" li:first");
-	if (first) {
-		first.remove();
-		$(elem+" ul").append(first);
-	}
-}
+var Scroller = function(elem, name, interval) {
+    this.elem = elem;
+    this.name = name;
+    if (!interval) interval = 10000;
+    this.interval = interval;
+    this.setTimeout();
+};
 
-function rotateBackward(elem) {
-	var last = $(elem+" li:last");
-	if (last) {
-		last.remove();
-		var first = $(elem+" li:first");
-		if (first)
-			$(first).before(last);
-		else
-			$(elem+" ul").append(last);
-	}
-}
-function startRotator(elem, interval) {
-	if (!interval) interval = 10000;
-	setInterval("rotateForward('"+elem+"')", interval);
-	//setInterval("rotateBackward('"+elem+"')", interval);
-}
+Scroller.prototype.setTimeout = function() {
+    if (this.timmer) clearTimeout(this.timmer);
+    this.timmer = setTimeout(this.name+".rotateForward()", this.interval);
+};
+
+Scroller.prototype.rotateForward = function() {
+    this.setTimeout();
+    var first = $(this.elem+" li:first");
+    if (first) {
+        first.remove();
+        $(this.elem+" ul").append(first);
+    }
+};
+
+Scroller.prototype.rotateBackward = function() {
+    this.setTimeout();
+    var last = $(this.elem+" li:last");
+    if (last) {
+        last.remove();
+        var first = $(this.elem+" li:first");
+        if (first)
+            $(first).before(last);
+        else
+            $(this.elem+" ul").append(last);
+    }
+};
